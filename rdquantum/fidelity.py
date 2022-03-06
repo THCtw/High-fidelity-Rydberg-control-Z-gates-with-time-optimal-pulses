8a3944302ffab585b9fe14e8d78e7b0119bec5ae
Tzu Hsuan, Chang <everybody3463@gmail.com>
2022-03-06 21:39:28 +0800

    Update Bell state fidelity

--- rdquantum/fidelity.py
+++ rdquantum/fidelity.py
@@ -1,4 +1,6 @@
+import re
 from qutip import *
+import numpy as np
 
 class fidelity:
     def __init__(self, GateOp):
@@ -6,4 +8,8 @@ class fidelity:
 
     def get_fidelity(self, Pulses, rho_init, rho_targets):
         results = self.GateOp(Pulses, rho_init, rho_targets)
-        return  results.expect[0][-1]
+        bell1 = results.expect[0][-1]
+        bell2 = results.expect[1][-1]
+        bell3 = results.expect[2][-1]
+        F = 1/2 * (bell1 + bell2) + np.absolute(bell3)
+        return  F
