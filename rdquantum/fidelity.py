b365de6638e9c2e3479b977cd299e18069a85b28
yoyo-tv <100761364+yoyo-tv@users.noreply.github.com>
2022-03-28 17:40:49 +0800

    Update fidelity.py

--- rdquantum/fidelity.py
+++ rdquantum/fidelity.py
@@ -7,9 +7,5 @@ class fidelity:
         self.GateOp = GateOp
 
     def get_fidelity(self, Pulses, rho_init, rho_targets):
-        results = self.GateOp(Pulses, rho_init, rho_targets)
-        bell1 = results.expect[0][-1]
-        bell2 = results.expect[1][-1]
-        bell3 = results.expect[2][-1]
-        F = 1/2 * (bell1 + bell2) + np.absolute(bell3)
+        F = self.GateOp(Pulses, rho_init, rho_targets)
         return  F
